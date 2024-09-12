bb9799e0edadf1642b7ee31939c842a44e7c5eff
olehhapuk <oleggapuk20@gmail.com>
2024-09-12 14:38:01 +0300

    added hubs and rooms

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HubsModule } from './hubs/hubs.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, HubsModule],
+  imports: [AuthModule, UsersModule, HubsModule, RoomsModule, MessagesModule],
   providers: [],
 })
 export class AppModule {}
--- src/hubs/hubs.controller.ts
+++ src/hubs/hubs.controller.ts
@@ -12,10 +12,14 @@ import {
 import { HubsService } from './hubs.service';
 import { ZodValidationPipe } from '@/zod-validation/zod-validation.pipe';
 import { CreateHubDTO, createHubSchema } from './dto/create-hub.dto';
+import { RoomsService } from '@/rooms/rooms.service';
 
 @Controller('hubs')
 export class HubsController {
-  constructor(private readonly hubsService: HubsService) {}
+  constructor(
+    private readonly hubsService: HubsService,
+    private readonly roomsService: RoomsService,
+  ) {}
 
   @Post(':hubId/join')
   @UseGuards(AuthGuard)
@@ -48,5 +52,11 @@ export class HubsController {
   async getJoined(@UserId() userId: number) {
     const hubs = await this.hubsService.getJoinedHubs(userId);
     return hubs;
+  }
+
+  @Get(':hubId/rooms')
+  async getRooms(@Param('hubId', ParseIntPipe) hubId: number) {
+    const hubRooms = await this.roomsService.findByHubId(hubId);
+    return hubRooms;
   }
 }
