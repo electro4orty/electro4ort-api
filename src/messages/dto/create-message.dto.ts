88775aa811f6bf26e11e1f19d40a39917e9c96a1
olehhapuk <oleggapuk20@gmail.com>
2024-11-09 22:39:14 +0200

    file uploads

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -4,6 +4,16 @@ export const createMessageSchema = z.object({
   roomId: z.string(),
   userId: z.string(),
   body: z.string().max(255),
+  attachments: z
+    .array(
+      z.object({
+        fileName: z.string(),
+        mimeType: z.string(),
+        size: z.number().positive(),
+      }),
+    )
+    .max(99)
+    .nullable(),
 });
 
 export type CreateMessageDTO = z.infer<typeof createMessageSchema>;
