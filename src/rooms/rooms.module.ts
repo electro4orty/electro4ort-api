4324d45eb32c4c1bf8a1eee3b98e537aff80f0cd
olehhapuk <oleggapuk20@gmail.com>
2024-11-11 23:24:22 +0200

    typing status

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -4,9 +4,18 @@ import { RoomsService } from './rooms.service';
 import { DrizzleService } from '@/db/drizzle.service';
 import { HubsService } from '@/hubs/hubs.service';
 import { MessagesService } from '@/messages/messages.service';
+import { RoomsGateway } from './rooms.gateway';
+import { UsersService } from '@/users/users.service';
 
 @Module({
   controllers: [RoomsController],
-  providers: [RoomsService, DrizzleService, HubsService, MessagesService],
+  providers: [
+    RoomsService,
+    DrizzleService,
+    HubsService,
+    MessagesService,
+    RoomsGateway,
+    UsersService,
+  ],
 })
 export class RoomsModule {}
