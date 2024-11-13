2ff4877a7738cee8c93b369d338f5194bee2d1bc
olehhapuk <oleggapuk20@gmail.com>
2024-11-13 18:12:32 +0200

    avatar upload

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const updateUserSchema = z.object({
   username: z.string().min(1).max(80),
   displayName: z.string().min(1).max(80),
-  avatar: z.string().url().or(z.string().max(0)),
+  avatar: z.string(),
   birthDate: z.string(),
 });
 
