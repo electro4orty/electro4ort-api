206a60d5cf2e81702df99ab9589e62b7a502554d
Oleh Hapuk <oleggapuk20@gmail.com>
2024-12-03 23:12:51 +0200

    fix: reliably display user status (#4)

--- src/users/users.gateway.ts
+++ src/users/users.gateway.ts
@@ -1,12 +1,15 @@
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { UsersService } from './users.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@/auth/auth.guard';
 import { debounce, DebouncedFunc } from 'lodash';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
@@ -14,6 +17,8 @@ import { debounce, DebouncedFunc } from 'lodash';
   },
 })
 export class UsersGateway {
+  @WebSocketServer() server!: Server;
+
   constructor(private readonly usersService: UsersService) {}
 
   setOfflineFns: Record<
@@ -24,14 +29,36 @@ export class UsersGateway {
   @SubscribeMessage('ping')
   @UseGuards(AuthGuard)
   async handlePing(@MessageBody('userId') userId: string) {
-    await this.usersService.updateStatus(userId, 'online');
+    const server = this.server;
+    let user = await this.usersService.updateStatus(userId, 'online');
+    server.to(`userStatusUpdate_${userId}`).emit('userStatusUpdate', user);
+
     if (!this.setOfflineFns[userId]) {
       this.setOfflineFns[userId] = debounce(async (userId: string) => {
-        await this.usersService.updateStatus(userId, 'offline');
+        user = await this.usersService.updateStatus(userId, 'offline');
+        server.to(`userStatusUpdate_${userId}`).emit('userStatusUpdate', user);
         delete this.setOfflineFns[userId];
       }, 5000);
     }
 
     this.setOfflineFns[userId](userId);
+  }
+
+  @SubscribeMessage('subscribeUserStatusUpdate')
+  @UseGuards(AuthGuard)
+  async handleSubscribeUserStatusUpdate(
+    @MessageBody('userId') userId: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.join(`userStatusUpdate_${userId}`);
+  }
+
+  @SubscribeMessage('unsubscribeUserStatusUpdate')
+  @UseGuards(AuthGuard)
+  async handleUnsubscribeUserStatusUpdate(
+    @MessageBody('userId') userId: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.leave(`userStatusUpdate_${userId}`);
   }
 }
